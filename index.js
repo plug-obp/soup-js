359ad21c9abf9ca4fab6959f5c49a6896005111a
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2024-02-06 13:38:07 +0100

    export core functions

---
+++ index.js
@@ -0,0 +1,4 @@
+import { link, readSoup } from './SoupReader';
+import { Environment, SoupSemantics, evaluateString, evaluateStepString } from './SoupSemantics';
+
+export { link, readSoup, Environment, evaluateString, evaluateStepString, SoupSemantics };
